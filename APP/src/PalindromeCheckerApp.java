31106f0d47c7d8ab287aa28c400b9cf307b5fc88
Uganesh087 <ugneshbabu41@gmail.com>
2026-03-29 00:28:34 +0530

    Uganesh Added UC5

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,22 +1,30 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp
 {
     public static void main(String args[])
     {
-        System.out.println("Welcome to The Palindrome Checker");
+        System.out.println("Version: 5.0");
         String txt = "123321";
-        char str[] = txt.toCharArray();
-        int n = str.length;
-        int m = n-1;
-        int c = 0;
-        for(int i = 0; i<n; i++)
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < txt.length(); i++)
         {
-            if(str[i]==str[m])
-                c++;
-            m--;
+            stack.push(txt.charAt(i));
         }
-        if(c==n)
-            System.out.println("The String "+txt+" is a Palindrome");
+
+        String reversed = "";
+
+        while(!stack.isEmpty())
+        {
+            reversed = reversed + stack.pop();
+        }
+
+        System.out.println("Original String: " + txt);
+        System.out.println("Reversed String: " + reversed);
+
+        if(txt.equals(reversed))
+            System.out.println("The String " + txt + " is a Palindrome");
         else
-            System.out.println("The String "+txt+" is Not a Palindrome");
+            System.out.println("The String " + txt + " is Not a Palindrome");
     }
 }
