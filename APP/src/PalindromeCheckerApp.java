019c01a187f0cd22ce0e3fa4bf1b413c0fa0571e
Uganesh087 <ugneshbabu41@gmail.com>
2026-03-29 00:23:34 +0530

    Uganesh Added UC4

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,31 +1,20 @@
-/*
-================================================================================================================
-MAIN CLASS - UseCase3PalindromeApp
-================================================================================================================
-
-Use Case 3: Reverse a String
-
-Description:
-This class represents basic palindrome validation by reversing a string and checking if it's
-palindrome
-
-
-The goal is to find if a Hardcoded string is Palindrome or not.
-
-@author 1346-pranav-io
-@version 2.0
- */
-
 public class PalindromeCheckerApp
 {
     public static void main(String args[])
     {
         System.out.println("Welcome to The Palindrome Checker");
         String txt = "123321";
-        String rev = new StringBuilder(txt).reverse().toString();
-        System.out.println("The Original String : "+txt);
-        System.out.println("The Reversed String : "+txt);
-        if(txt.equals(rev))
+        char str[] = txt.toCharArray();
+        int n = str.length;
+        int m = n-1;
+        int c = 0;
+        for(int i = 0; i<n; i++)
+        {
+            if(str[i]==str[m])
+                c++;
+            m--;
+        }
+        if(c==n)
             System.out.println("The String "+txt+" is a Palindrome");
         else
             System.out.println("The String "+txt+" is Not a Palindrome");
